2208f04ce3753001fde15f5501f9b07587f3771e
AmoxV2 <60665323+AmoxV2@users.noreply.github.com>
2024-09-10 13:58:55 +0200

    Trying tol Scrape more From website

--- likedinScraper.py
+++ likedinScraper.py
@@ -4,7 +4,7 @@ import pandas as pd
 import time
 import random
 
-def linkedin_scraper(tittle="RPA", location="Poland", how_pages=3):
+def linkedin_scraper(tittle="RPA", location="Poland", how_pages=0):
     
     num_page = 0
     list_page_jobs = []
@@ -45,13 +45,11 @@ def linkedin_scraper(tittle="RPA", location="Poland", how_pages=3):
                 print(f"Coun Check {Checking} this {job_id} time sleep {time_sleep}")
                 time.sleep(time_sleep)
                 Checking +=1
-                   
-                    
-                       
-                   
-       
-        
+                
         job_soup = BeautifulSoup(job_response.text,"html.parser")
+        with open("job_soup.txt", "a", encoding="utf-8") as file:
+            file.write(str(job_soup)+"\n")
+        
         job_post= {}
         
         job_post["job_link"] = job_url
@@ -64,14 +62,26 @@ def linkedin_scraper(tittle="RPA", location="Poland", how_pages=3):
             job_post["company_name"] = job_soup.find("a", {"class":"topcard__org-name-link topcard__flavor--black-link"}).text.strip()
         except:
             job_post["company_name"]  = None
-        try:    
+        try:           
             job_post["time_posted"] = job_soup.find("span", {"class":"posted-time-ago__text topcard__flavor--metadata"}).text.strip()
         except:
             job_post["time_posted"]  = None
         try:    
-            job_post["num_applicatns"] = job_soup.find("span", {"class":"num-applicants__caption topcard__flavor--metadata topcard__flavor--bullet"}).text.strip()
-        except:   
-            job_post["num_applicatns"]  = None
+            if job_soup.find("span", {"class":"num-applicants__caption topcard__flavor--metadata topcard__flavor--bullet"}).text.strip():
+                job_post["num_applicatns"] = job_soup.find("span", {"class":"num-applicants__caption topcard__flavor--metadata topcard__flavor--bullet"}).text.strip()
+                print(job_post["num_applicatns"])
+            else:
+                job_post["num_applicatns"] = job_soup.find("span", {"figcaption":"num-applicants__caption"}).text.strip()
+                print(job_post["num_applicatns"])
+        except: 
+            print("Error")  
+            job_post["num_applicatns"]  = None 
+        # try:    
+        #     job_post["num_applicatns"] = job_soup.find("span", {"figcaption":"num-applicants__caption"}).text.strip()
+        #     print(job_soup.find("span", {"figcaption":"num-applicants__caption"}).text.strip())
+        # except:   
+        #      job_post["num_applicatns"]  = None
+        
         job_list.append(job_post)
             
            
