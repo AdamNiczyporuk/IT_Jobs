d678d6240903a94c0f276aeb36f9d5a835870688
AmoxV2 <60665323+AmoxV2@users.noreply.github.com>
2024-09-17 12:14:04 +0200

    Add To diffrent function One for Likedin one for nofluffy jobs

--- ManageDB.py
+++ ManageDB.py
@@ -67,7 +67,7 @@ def create_tableDB(nameDB):
             connection.close()
             print("Połączenie zostało zamknięte.")
 
-def saveToDB(data,nameDB):
+def saveToDBLikedin(data,nameDB):
    try:
         connection  = pymysql.connect(
             host='127.0.0.1',
@@ -81,6 +81,54 @@ def saveToDB(data,nameDB):
         
         InserQuery = """INSERT INTO LinkedInJobs (job_id,job_title, company_name,City, time_posted, job_link,num_applicatns, formatted_dataTime) VALUES (%s,%s,%s,%s,%s, %s, %s, %s)"""
         SelectQuery="""SELECT COUNT(*) From LinkedInJobs WHERE job_id=%s"""
+        
+        
+        for job in data:
+            job_id = job.get('job_id')
+            job_title = job.get('job_title')
+            Company_name = job.get('company_name')
+            city = job.get('city').split(",")[0]
+            time_posted = job.get('time_posted')
+            job_link = job.get('job_link')
+            num_applicatns= job.get('num_applicatns')
+           
+            dataTime=datetime.datetime.now()
+            formatted_dataTime = dataTime.strftime("%Y-%m-%d %H:%M:%S")
+            
+            cursor.execute(SelectQuery, (job_id,))
+            result = cursor.fetchone()
+            
+            if result[0] == 0:
+                cursor.execute(InserQuery, (job_id,job_title, Company_name,city, time_posted, job_link,num_applicatns, formatted_dataTime))
+        
+        connection.commit()
+        
+        
+   except pymysql.MySQLError as error:
+        print(f"Error: {error}")
+        
+        
+   finally:
+       if connection: 
+           cursor.close()
+           connection.close()
+           print("Połączenie zostało zamknięte.")
+
+
+def saveToDBScraper(data,nameDB):
+   try:
+        connection  = pymysql.connect(
+            host='127.0.0.1',
+            user=config_data.user,
+            password=config_data.password,
+            database=nameDB,
+            port= 3306
+        )
+        cursor = connection .cursor()
+        
+        
+        InserQuery = """INSERT INTO LinkedInJobs (id,job_title,job_link,site,type, formatted_dataTime) VALUES (%s,%s,%s,%s,%s,%s)"""
+        SelectQuery="""SELECT COUNT(*) From job_link WHERE job_id=%s"""
         
         
         for job in data:
