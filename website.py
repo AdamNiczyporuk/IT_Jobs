f8bbadee270c11329f83d566e5c1ddbd092f3f10
AmoxV2 <60665323+AmoxV2@users.noreply.github.com>
2024-09-24 16:17:23 +0200

    Add mainPage where people will write location and category and serach on another sites

--- website.py
+++ website.py
@@ -2,10 +2,14 @@ import ManageDB
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/jobs')
 def index ():
     data = ManageDB.get_dataDB("LinkedInDB")
     return render_template('index.html', jobListing=data)
+
+@app.route('/')
+def main_page():
+    return render_template('mainpage.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
