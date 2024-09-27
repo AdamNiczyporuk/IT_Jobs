4e019ad840f408c4c2084587039eb86148887cdd
AmoxV2 <60665323+AmoxV2@users.noreply.github.com>
2024-09-27 21:43:14 +0200

    trying to fix animation

--- static/mainpage.js
+++ static/mainpage.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const typingText = document.getElementById("typing-text");
+    const typingDots = document.getElementById("typing-dots");
     const text = typingText.textContent;
     typingText.textContent = "";
 
@@ -9,7 +10,38 @@ document.addEventListener("DOMContentLoaded", function() {
             typingText.textContent += text.charAt(index);
             index++;
             setTimeout(type, 100);
+        } else {
+            setTimeout(rewriteDots, 500);
         }
+    }
+
+    function rewriteDots() {
+        typingDots.textContent = "";
+        let dots = 0;
+        function addDot() {
+            if (dots < 3) {
+                typingDots.textContent += "?";
+                dots++;
+                setTimeout(addDot, 500);
+            } else {
+                setTimeout(removeDots, 500);
+            }
+        }
+        addDot();
+    }
+
+    function removeDots() {
+        let dots = typingDots.textContent.length;
+        function removeDot() {
+            if (dots > 0) {
+                typingDots.textContent = typingDots.textContent.slice(0, -1);
+                dots--;
+                setTimeout(removeDot, 500);
+            } else {
+                setTimeout(rewriteDots, 500);
+            }
+        }
+        removeDot();
     }
 
     type();
